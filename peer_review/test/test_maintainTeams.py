177d4b0313ad90ec6cbcff0090b06d564912053d
MatthewBotha <m.botha41@gmail.com>
2016-07-14 11:22:53 +0200

    Complete Setup

--- peer_review/test/test_maintainTeams.py
+++ peer_review/test/test_maintainTeams.py
@@ -5,10 +5,14 @@ from peer_review.models import User, TeamDetail, RoundDetail
 
 from peer_review.views import *
 import json
+import datetime
 
 class MaintainTeamTests(TestCase):
 	def setUp(self):
 		self.client = Client()
-        self.user1 = User.objects.create_user('bob@bob.com', 'bob')
-        self.user2 = User.objects.create_user('joe@joe.com', 'joe')
-		self.round1 = RoundDetail.objects.create(name="Round 1", )
+		self.user1 = User.objects.create_user('bob@bob.com', 'bob')
+		self.user2 = User.objects.create_user('joe@joe.com', 'joe')
+		self.round1 = RoundDetail.objects.create(name='Round 1', startingDate=datetime.datetime.now(), endingDate=datetime.datetime.now(), description='Test Round 1')
+		self.round2 = RoundDetail.objects.create(name='Round 2', startingDate=datetime.datetime.now(), endingDate=datetime.datetime.now(), description='Test Round 2')
+	
+	
